0fea8c770bb478bb3c721ab91ddcfc682599ecaa
ChadeBibbins <chade.bibbins@dillard.edu>
2018-02-01 16:42:41 -0800

    added more buttons

--- my_script.js
+++ my_script.js
@@ -1,12 +1,20 @@
 
 function sayHello() {
     alert("Hi Chade!");
-};
+}
 
 function sayHey() {
   alert("What's Up Aaliyah");
-};
+}
 
 function sayHi() {
   alert("Hola Armonn");
-};
+}
+
+function sayHola(name) {
+  alert("Hi, " +name);
+}
+
+function ow(){
+  alert("That Hurt!");
+}
